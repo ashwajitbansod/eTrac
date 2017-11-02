d37725680750c8a5151f6924df724f7aa47c9719
ashwajitbansod <aswajitbansod@gmail.com>
2017-11-02 17:08:52 +0530

    Final Jugad

---
+++ WorkOrderEMS.Models/UserModels/DriverModel_M.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOrderEMS.Models.UserModels
+{
+    public class DriverModel_M
+    {
+        public string DriverName { get; set; }
+        public string VehicleType { get; set; }
+        public string LicenseNo { get; set; }
+        public string DriverProfilePic { get; set; }
+        public string VehicleTagNo { get; set; }
+        public string VehicleImage { get; set; }
+        public string SpecialNotes { get; set; }
+        public string VehicleIdentificationNo { get; set; }
+        public string VendorName { get; set; }
+        public string BusinessNo { get; set; }
+    }
+}
